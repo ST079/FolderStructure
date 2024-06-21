a1bfde29326441527734e78187e455d62329e007
Sujan Tamang <suzanyba079@gmail.com>
2024-06-21 12:10:50 +0545

    methods

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const indexRouter = require("./routes");
+app.use(express.json());
 
 app.use("/", indexRouter);
 
--- routes/book_route.js
+++ routes/book_route.js
@@ -5,6 +5,14 @@ router.get("/", (req, res) => {
   res.json({ msg: "hello from book" });
 });
 
+router.post("/:id", (req, res) => {
+  //req.params => /:id => req.params.id // details
+  //req.query => ?key:value //paginations
+  //req.body => raw json ({'key':'value'}) //forms
+  console.log({ params: req.params });
+  console.log({ body: req.body });
+  console.log({ Query: req.query });
+});
 router.use("/users", userRouter);
 
 module.exports = router;
